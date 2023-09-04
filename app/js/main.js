d0fc2bde737e5a9b431f934354353f2d602cfafa
ArdroomMer <artikdrummer@icloud.com>
2023-09-04 18:41:56 +0200

    added sections, added mixitup, added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function(){
 
+  $('.slider__inner').slick({
+    dots: true,
+    arrows: true,
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><img src="../images/home_page/prev-arrow.png">',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><img src="../images/home_page/next-arrow.png">'
+    });
+
 });
+
+var mixer = mixitup(".categories__box");
